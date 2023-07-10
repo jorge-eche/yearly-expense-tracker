ddbc3eb6cf1663b806a7d4bf576b74b37587f843
Jorge Echeverria <coko.eche@gmail.com>
2023-07-10 20:01:43 +1000

    Code conditional layout of expenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = ({ expenses }) => {
+  const [year, setYear] = useState("2020");
+
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === year
+  );
+
+  let expenseContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem key={expense.id} expense={expense} />
+    ));
+  }
+
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem key={expense.id} expense={expense} />
-      ))}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter year={year} setYear={setYear} />
+        {expenseContent}
+      </Card>
+    </div>
   );
 };
 
