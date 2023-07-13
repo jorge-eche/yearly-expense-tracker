347d32497c383cf2084e9f1ef00885fd1ac88502
Jorge Echeverria <coko.eche@gmail.com>
2023-07-13 18:11:11 +1000

    Remove node_modules folder and add .gitignore

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,16 +1,21 @@
+import "./ChartBar.css";
+
 const ChartBar = ({ dataPoint }) => {
-  const { label, max, value. maxValue } = dataPoint;
+  const { label, max, value, maxValue } = dataPoint;
 
   let barFillHeight = "0%";
 
   if (max > 0) {
-    barFillHeight = Math.round((value / maxValue) * 100) + '%';
+    barFillHeight = Math.round((value / maxValue) * 100) + "%";
   }
 
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
-        <div className="chart-bar__fill" style={{height= barFillHeight}}></div>
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
       </div>
       <div className="chart-bar__label">{label}</div>
     </div>
